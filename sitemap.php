30bd6b37782cf8b5479ecc9f28c343287c5cac26
gama494 <148079441+gama494@users.noreply.github.com>
2025-10-05 13:23:32 +0200

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -1,16 +1,5 @@
 <?php
 header('Content-Type: application/xml');
-echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 echo '<url><loc>https://titchuke.com/</loc><lastmod>' . date('Y-m-d') . '</lastmod><changefreq>weekly</changefreq><priority>1.0</priority></url>';
-
-// Replace with your song data (e.g., from database or array)
-$songs = [
-    ['title' => 'SI INE', 'artist' => 'JAY FRO2'],
-    // Add more songs
-];
-foreach ($songs as $song) {
-    $url = urlencode("title={$song['title']}&artist={$song['artist']}");
-    echo "<url><loc>https://titchuke.com/download.html?{$url}</loc><lastmod>2025-10-05</lastmod><changefreq>monthly</changefreq><priority>0.8</priority></url>";
-}
-echo '</urlset>';
-?>
